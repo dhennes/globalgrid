8b562d857f3ca117fdb78a35af12170e015c4a58
Daniel Hennes <daniel.hennes@gmail.com>
2014-08-06 14:38:45 +0200

    initial commit

---
+++ grid_lp.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+import numpy as np
+import itertools
+from scipy.io import netcdf
+
+from cvxopt import matrix, solvers
+solvers.options['show_progress'] = False
+
+import PyGMO
+
+from utils import Timer
+
+
+def powerLoss(kms):
+    #assuming 800kV dC powerlines - approximataly 3.0% loss
+    # 2.0% is loss of convertors and begin/end
+    lossfrac = 2.0 + (6.0 / 2000.) * kms
+    return lossfrac / 100.
+
+
+def read_data():
+    print ">>> call: read_data()"
+    f = netcdf.netcdf_file("data_zones.nc", "r")
+    data = {}
+    #zone numbers
+    data["zones"] = f.variables['zones'][:]
+    #zone position - latitude
+    data["lats"] = f.variables['lat'][:]
+    #zone position - longtitude
+    data["lons"] = f.variables['lon'][:]
+    #time (GMT) = local solar time
+    data["time"] = f.variables['time'][:]
+    #average cloud fraction (zone,time)
+    data["clouds"] = f.variables['clouds'][:]
+    #average distance between zones (zone,zone)
+    data["distance"] = f.variables['distance'][:]
+    #average electricity demand zone (zone,time)
+    data["demand"] = f.variables['demand'][:]
+    #average solar irradiance zone (zone,time)
+    data["irradiance"] = f.variables['irradiance'][:]
+    # powerloss
+    data["powerloss"] = np.array(map(powerLoss, data["distance"]))
+    f.close()
+    return data
+
+    
+class grid_linear_problem(PyGMO.problem.base):
+
+    __data = read_data()
+
+    def __init__(self):
+        self.data = self.__data
+
+        # flow link indecies
+        self.fidx = list(itertools.product(self.data["zones"], self.data["zones"]))
+        for item in [(x, x) for x in self.data["zones"]]:
+            del self.fidx[self.fidx.index(item)]
+
+        # dimensions
+        self.N = len(self.data["zones"])
+        self.n = len(self.fidx)
+        dim = self.n / 2
+            
+        super(grid_linear_problem, self).__init__(dim, dim, 2, 0, 0, 0)
+        self.set_bounds(0, 1)
+        
+    def powerProd(self, cloud, irradiance):
+        # area in 1e6 km^2
+        return .2 * irradiance * cloud / 1e6
+
+    def getTotalDist(self, flow):
+        dist = 0
+        upper_idx = np.triu_indices(self.N, k=1)
+        upper_idx = zip(upper_idx[0], upper_idx[1])
+        for i in np.nonzero(flow)[0]:
+            dist += self.data["distance"][upper_idx[i][0], upper_idx[i][1]]
+        return dist
+        
+    def create_sub_matrix(self, t, flow):
+        N = len(self.data["zones"])
+        
+        # calculate production
+        cloud = self.data["clouds"][:, t]
+        irradiance = self.data["irradiance"][:, t]
+        # TODO: should be moved to init
+        production_coeff = np.array(map(lambda x: self.powerProd(*x), zip(cloud, irradiance)))
+
+        # demand
+        demand = self.data["demand"][:, t]  # in peta W
+
+        n = len(self.fidx)  # flow indices
+    
+        # A
+        A_balance = np.zeros((N, N+n))
+        for i in self.data["zones"]:
+            # production
+            A_balance[i, i] = -1 * production_coeff[i] 
+            
+            # flow
+            for j, (f_from, f_to) in enumerate(self.fidx):
+                if flow[j]:
+                    if i == f_from:
+                        # outgoing
+                        A_balance[i, N+j] = 1
+                    if i == f_to:
+                        # incoming
+                        A_balance[i, N+j] = -self.data["powerloss"][f_from, f_to]
+                    
+        A_area_pos = np.hstack([np.identity(N), np.zeros((N, n))]) * -1
+        A_link_pos = np.hstack([np.zeros((n, N)), np.identity(n)]) * -1
+        A = np.vstack([A_area_pos, A_link_pos, A_balance])
+        
+        # b
+        b_area_pos = np.array([0] * N)
+        b_link_pos = np.array([0] * n)
+        b_balance = np.array(demand) * -1  # -p <= -d  <=>  p >= d
+        b = np.hstack([b_area_pos, b_link_pos, b_balance])
+        
+        return (A, b)
+        
+    def solve(self, flow):
+        N = len(self.data["zones"])
+        As_area = []
+        As_flow = []
+        bs = []
+        for t in self.data["time"]:
+            (A, b) = self.create_sub_matrix(t, flow)
+            # seperate area and flow part of sub problem
+            A = A[N:, :]  # remove area_pos part
+            A_area = A[:, 0:N]  # split in area
+            A_flow = A[:, N:]   # ... and flow
+            As_area.append(A_area)
+            As_flow.append(A_flow)
+
+            b = b[N:] # remove area_pos part
+            bs.append(b)
+
+        from scipy.linalg import block_diag
+        A_flow = block_diag(*As_flow)
+        A_flow = np.vstack([np.zeros((N, A_flow.shape[1])), A_flow])  # add N rows of 0's on top
+
+        A_area = np.vstack(As_area)
+        A_area = np.vstack([np.identity(N) * -1, A_area])  # add area_pos constraint
+
+#        print A_area.shape
+#        print A_flow.shape
+
+        A = np.hstack([A_area, A_flow])
+        
+        b = np.concatenate(bs)
+        b = np.concatenate([np.array([0] * N), b])  # add area_pos constraints
+
+#        print A.shape
+#        print b.shape
+
+        c = np.array([1.] * N + [0.] * (A.shape[1]-N))
+        
+        # solve
+        A = matrix(A)
+        b = matrix(b)
+        c = matrix(c)
+        sol = solvers.lp(c, A ,b)
+        return sol
+
+    def _objfun_impl(self, x):
+        # transform upper triangular flow indices to full list
+        upper_idx = np.triu_indices(self.N, k=1)
+        upper_idx = zip(upper_idx[0], upper_idx[1])
+    
+        flow = [False] * self.n
+        for (f, (i, j)) in zip(x, upper_idx):
+            if f:
+                flow[self.fidx.index((i, j))] = True
+                flow[self.fidx.index((j, i))] = True
+                
+        sol = self.solve(flow)
+
+        if sol["status"] == "optimal":
+            # objectives
+            f_area = sum(sol["x"][:self.N]) 
+            f_links = self.getTotalDist(x)
+        else:
+            f_area = float("inf")
+            f_links = float("inf")
+
+        print "    area: %.2f    length: %.2f    links: %d" % (f_area, f_links, len(np.nonzero(x)[0]))
+        return (f_area, f_links)
+        
+if __name__ == "__main__":
+    import pickle
+    
+    prob = grid_linear_problem()
+    print prob
+    pop = PyGMO.population(prob, 49)
+    pop.push_back([True] * prob.dimension)
+    algo = PyGMO.algorithm.ihs(1)
+    for i in xrange(1000):
+        with Timer(verbose=True) as t:
+            pop = algo.evolve(pop)
+#        print pop.champion.f
+#        print pop.champion.x
+
+        # save generation
+        fname = "results/gen-%d.pickle" % i
+        with open(fname, "wb") as f:
+            pickle.dump(pop, f)
+        
+
